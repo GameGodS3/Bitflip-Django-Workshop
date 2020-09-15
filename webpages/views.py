f035e336b93ae6e8e4c70769d68940d7ca35d1eb
gamegods3 <sudevssuresh@gmail.com>
2020-09-15 18:32:00 +0530

    Complete without json processing

--- webpages/views.py
+++ webpages/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .forms import EventCreator
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'webpages/index.html', )
+    return render(request, 'webpages/index.html')
+
+def events(request):
+    form = EventCreator(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            evetitle = form.cleaned_data['eventtitle']
+            evedesc = form.cleaned_data['eventdesc']
+            print(evetitle, evedesc)
+            return render(request, 'webpages/events.html', {'form':form})
+    else:
+        return render(request, 'webpages/events.html', {'form':form})
+        
